41b728c98ba5612728ac208cb1c07118250fbc17
bilal.alnaani@fh-erfurt.de <bilal0710>
2020-05-20 21:43:21 +0200

    mein erster Code mit node.js

---
+++ index.js
@@ -0,0 +1,17 @@
+ /**
+  * @author Bilal Alnaani
+  * @version 1.0.0
+  * @description index.js is our launch start point to initialize the application with all needs.
+  */
+
+ const express = require("express");
+ const app = express();
+ const http = require("http").createServer(app);
+
+ app.get('/', (req, res) => {
+     console.log("Hello World!");
+ });
+
+ http.listen(3000, function() {
+     console.log('App listing at http://localhost:3000');
+ });
